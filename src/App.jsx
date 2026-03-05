0bca905670fc0945b11933fb694af3e6aebc2846
faheemahmed24 <faheemahmadburoojwork@gmail.com>
2026-03-05 21:21:46 +0500

    Create src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Footer from "./components/Footer"
+
+function App(){
+
+return(
+
+<div>
+
+<Header/>
+
+<Home/>
+
+<Footer/>
+
+</div>
+
+)
+
+}
+
+export default App
